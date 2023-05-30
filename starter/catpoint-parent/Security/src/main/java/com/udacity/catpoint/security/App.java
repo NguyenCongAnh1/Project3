7b990054f5a67a8a782c9e730e4636d5162f673e
NguyenCongAnh1 <nguyenanh342000@gmail.com>
2023-05-30 22:00:47 +0700

    2th commit

---
+++ starter/catpoint-parent/Security/src/main/java/com/udacity/catpoint/security/App.java
@@ -0,0 +1,14 @@
+package com.udacity.catpoint.image;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
---
+++ starter/catpoint-parent/Security/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module Security {
+    requires Image;
+    requires com.google.gson;
+    requires java.prefs;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires miglayout;
+    requires com.google.common;
+}
